42fe1de6eacda61689bb53c2a9727e0c87b65dee
svhenrique <silviohenrique1234@hotmail.com>
2021-07-06 09:59:18 -0300

    added owner in SnippetSerializer

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -30,9 +30,11 @@ class SnippetSerializer(serializers.Serializer):
 '''
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
 
 class UserSerializer(serializers.ModelSerializer):
     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
